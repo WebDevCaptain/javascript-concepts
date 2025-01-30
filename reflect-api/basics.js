339dfc37adf9ee5376d0e050ab76c35e8eb2c422
Shreyash <hello@webdevcaptain.com>
2025-01-30 06:46:00 +0000

    Calling functions with specific this context, checking and modifying prototypes of an object

--- reflect-api/basics.js
+++ reflect-api/basics.js
@@ -21,7 +21,7 @@ const myself = {
 };
 
 // 1. Getting a property value ------------------------------
-console.log(Reflect.get(myself, "name"), Reflect.get(myself, "age"));
+console.log(Reflect.get(myself, "name"), Reflect.get(myself, "age")); // Shreyash 50
 
 // 2. Setting an object's property ------------------------------
 Reflect.set(myself, "age", 51);
@@ -40,4 +40,38 @@ Reflect.deleteProperty(myself, "isAdmin");
 console.log(myself);
 
 // 5. Listing all the properties (non-inherited) of an object  ------------------------------
-console.log("My keys", Reflect.ownKeys(myself));
+console.log("My keys", Reflect.ownKeys(myself)); // [ 'name', 'age' ]
+
+// 6. Calling a function with a specific this value  ------------------------------
+const user = {
+  name: "Shreyash",
+  age: 50,
+};
+
+function sayHello(greeting1, greeting2) {
+  return `Hello, ${this.name}!. ${greeting1}, ${greeting2}`;
+}
+
+const val = Reflect.apply(sayHello, user, [
+  "How are you?",
+  "Hope you are doing well.",
+]); // Hello, Shreyash!. How are you?, Hope you are doing well.
+
+console.log(val);
+
+// 7. Checking and modifying prototypes  ------------------------------
+const person = {
+  role: "User",
+};
+const admin = {
+  permissions: ["read", "write"],
+};
+
+Reflect.setPrototypeOf(admin, person); // admin.__proto__ = person
+
+console.log("Admin role:", admin.role); // User
+console.log("Admin permissions", admin.permissions); // ["read", "write"]
+
+console.log(Reflect.getPrototypeOf(admin), admin.__proto__); // { role: 'User' } { role: 'User' }
+
+console.log(Reflect.ownKeys(admin)); // ['permissions']
